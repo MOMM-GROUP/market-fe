509d38805d76e799133d68a1c9b9d8d3b6600a46
nachoshaman <kalmpurcell@gmail.com>
2025-10-02 11:28:47 -0700

    fied layous

--- app/layout.tsx
+++ app/layout.tsx
@@ -109,11 +109,12 @@ export default async function RootLayout({
           {(process.env.NODE_ENV === "development" || process.env.VERCEL_ENV === "preview") && (
             // eslint-disable-next-line @next/next/no-sync-scripts
             <script
-              data-recording-token={process.env.METICULOUS_DATA_RECORDING_TOKEN}
+              data-recording-token="ZOxGEtX1Mv1sxDjLJW07CAgsLPt2nu0jT2IzdBqD"
               data-is-production-environment="false"
               src="https://snippet.meticulous.ai/v1/meticulous.js"
             />
           )}
+        {/* Use the new Providers wrapper here */}
         <Providers serverSession={user}>
           <div className="min-h-screen flex flex-col">
             <Suspense fallback={<div>Loading...</div>}>
